14711685546aae81cc04b3963273241bae261f47
mn1490949269 <1490949269@qq.com>
2019-08-26 21:35:11 +0800

    gameGuessNumber

--- src/main/java/tw/Main.java
+++ src/main/java/tw/Main.java
@@ -8,11 +8,19 @@ import tw.commands.GuessInputCommand;
 public class Main {
 
     private static final int RUN_TIMES = 6;
+    private static final char answer[] = {'1','2','3','4'};
 
     public static void main(String[] args) throws Exception {
         for (int i = 0; i < RUN_TIMES; i++) {
             String input = new GuessInputCommand().input();
-            System.out.println(input);
+            GuessNumber  guessNumber = new GuessNumber();
+            String inputMessage = guessNumber.isValid(input);
+            if(inputMessage == "RightInput") {
+            	String output  = guessNumber.guess(input, answer);
+            	System.out.println(output);
+            }else {
+            	System.out.println(inputMessage);
+            }
         }
     }
 }
